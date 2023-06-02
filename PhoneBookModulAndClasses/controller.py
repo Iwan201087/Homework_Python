8a62e39d98a1360b2327cc14e86ebfdd075a4e2b
Iwan Yudzitski <iwan201087@gmail.com>
2023-06-02 16:21:54 +0300

    Домашняя работа к Семинару №8

---
+++ PhoneBookModulAndClasses/controller.py
@@ -0,0 +1,55 @@
+import text
+import view
+import model
+
+my_pb = model.PhoneBook()
+
+def start():
+    while True:
+        choice = view.main_menu()
+
+        match choice:
+            case 1: # открыть файл
+                my_pb.open()
+                view.print_message(text.load_successful)
+            case 2: # сохранить файл
+                my_pb.save()
+                view.print_message(text.save_successful)
+            case 3: # показать контакты
+                pb = my_pb.load()
+                view.print_contacts(pb, text.pb_empty)
+            case 4: # добавить контакт
+                contact = view.input_contact(text.input_new_contact)
+                name = my_pb.add(contact)
+                view.print_message(text.new_contact_successful(name))
+            case 5: # найти контакт
+                key_word = view.input_search(text.input_search)
+                result = my_pb.search(key_word)
+                view.print_contacts(result, text.empty_search(key_word))
+            case 6: # изменить контакт
+                key_word = view.input_search(text.input_change)
+                result = my_pb.search(key_word)
+                if result:
+                    if len(result) != 1:
+                        view.print_contacts(result, '')
+                        current_id = view.input_search(text.input_index)
+                    else:
+                        current_id = result[0].get('id')
+                    new_contact = view.input_contact(text.change_contact)
+                    name = my_pb.change(new_contact, current_id)
+                    view.print_message(text.change_successful(name))
+                else:
+                    view.print_message(text.empty_search(key_word))
+
+            case 7: # удалить контакт
+                key_word = view.input_search(text.input_delete_contact)
+                result = my_pb.search(key_word)
+                view.print_contacts(result, text.empty_search(key_word))
+                index_delete = view.input_search(text.input_delete_contact_true)
+                result = my_pb.delete(index_delete)
+                view.print_message(text.delete_successful)
+                my_pb.save()
+
+            case 8: # выйти и спросить о сохранении
+                my_pb.ask_user_for_save_pb()
+                break
---
+++ PhoneBookModulAndClasses/model.py
@@ -0,0 +1,56 @@
+import text
+class PhoneBook:
+    def __init__(self, path: str = 'phones.txt'):
+        self.phone_book: list[dict[str, str]] = []
+        self.path = path
+    def open(self):
+        with open(self.path, 'r', encoding='UTF-8') as file:
+            data = file.readlines()
+        for contact in data:
+            contact = contact.strip().split(':')
+            new = {'id': contact[0], 'name': contact[1], 'phone': contact[2], 'comment': contact[3]}
+            self.phone_book.append(new)
+    def save(self):
+        data = []
+        for contact in self.phone_book:
+            data.append(':'.join([contact['id'], contact['name'], contact['phone'], contact['comment']]))
+        data = '\n'.join(data)
+        with open(self.path, 'w', encoding='UTF-8') as file:
+            file.write(data)
+    def load(self):
+        return self.phone_book
+    def add(self, new: dict[str, str]) -> str:
+        new_id = int(self.phone_book[-1].get('id')) + 1
+        new['id'] = str(new_id)
+        self.phone_book.append(new)
+        return new.get('name')
+    def search(self, word: str) -> list[dict[str, str]]:
+        result: list[dict[str, str]] = []
+        for contact in self.phone_book:
+            for field in contact.values():
+                if word.lower() in field.lower():
+                    result.append(contact)
+                    break
+        return result
+    def change(self, new: dict, index: int) -> str:
+        for contact in self.phone_book:
+            if index == contact.get('id'):
+                contact['name'] = new.get('name', contact.get('name'))
+                contact['phone'] = new.get('phone', contact.get('phone'))
+                contact['comment'] = new.get('comment', contact.get('comment'))
+                return contact.get('name')
+    def delete(self, index_delete: str) -> list[dict[str, str]]:
+        for contact in self.phone_book:
+            for field in contact.values():
+                if index_delete.lower() in field.lower():
+                    self.phone_book.remove(contact)
+                    break
+        return self.phone_book
+    def ask_user_for_save_pb(self):
+        while True:
+            save_or_not = input(text.save_or_not)
+            if save_or_not == 'y':
+                self.save()
+                break
+            elif save_or_not == 'n':
+                break
